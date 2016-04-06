40aaf4e81f3b092d8d329af86d8c4c0325d56a9d
Nick <nick@Nicks-MBP.fios-router.home>
2016-04-06 18:50:48 -0400

    Testing Glib

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <libircclient.h>
 #include <libirc_rfcnumeric.h>
+#include <glib.h>
+#include <glib/gstring.h>
+#include <glib/gprintf.h>
 
 int main(void)
 {
-    printf("Haiiii\n\n\n");
+    g_printf("Haiiii\n\n\n");
     return 0;
 }
 
